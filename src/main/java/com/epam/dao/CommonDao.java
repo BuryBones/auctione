656811f13f049f172f18dc404b5ed470de4b462d
Bury_Bones <derkach.a.d@gmail.com>
2021-01-28 16:46:23 +0400

    fix findByIdWithAttributes() method to accept both Singular and Plural Attributes

--- src/main/java/com/epam/dao/CommonDao.java
+++ src/main/java/com/epam/dao/CommonDao.java
@@ -7,7 +7,9 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Root;
-import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.Attribute;
+import javax.persistence.metamodel.PluralAttribute;
+import javax.persistence.metamodel.SingularAttribute;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -24,40 +26,26 @@ public interface CommonDao<T> {
     return Optional.ofNullable(result);
   }
 
-  default Optional<T> findByIdWithFields(int id, String... fields) {
+  default Optional<T> findByIdWithAttributes(int id, Attribute<T,?>... fields)
+      throws IllegalArgumentException{
     Session session = HibernateUtil.getSessionFactory().openSession();
     CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
     CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(getType());
 
     Root<T> root = criteriaQuery.from(getType());
     if (fields != null && fields.length > 0) {
-      for (String field: fields) {
-        root.fetch(field, JoinType.LEFT);
+      for (Attribute<T, ?> field: fields) {
+        if (field instanceof SingularAttribute) {
+          SingularAttribute<T, ?> attribute = (SingularAttribute<T, ?>) field;
+          root.fetch(attribute, JoinType.LEFT);
+        } else if (field instanceof PluralAttribute) {
+          PluralAttribute<T, ?, ?> attribute = (PluralAttribute<T, ?, ?>) field;
+          root.fetch(attribute, JoinType.LEFT);
+        } else {
+          throw new IllegalArgumentException(field + " is neither Singular nor Plural Attribute.");
+        }
       }
     }
-
-    criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("id"), id));
-
-    Query<T> query = session.createQuery(criteriaQuery);
-    query.setMaxResults(1);
-    List<T> result = query.getResultList();
-
-    session.close();
-    return result.isEmpty() ? Optional.empty() : Optional.ofNullable(result.get(0));
-  }
-
-  default Optional<T> findByIdWithAttributes(int id, SetAttribute<T,?> fields) {
-    Session session = HibernateUtil.getSessionFactory().openSession();
-    CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-    CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(getType());
-
-    Root<T> root = criteriaQuery.from(getType());
-    root.fetch(fields,JoinType.LEFT);
-//    if (fields.length > 0) {
-//      for (SetAttribute<User,?> field: fields) {
-//        root.fetch(field, JoinType.LEFT);
-//      }
-//    }
 
     criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("id"), id));
 
