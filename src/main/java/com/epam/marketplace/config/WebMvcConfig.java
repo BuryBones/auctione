7441d312bc809b56eef372b0f34dd116c9651305
Bury_Bones <derkach.a.d@gmail.com>
2021-03-05 14:33:20 +0400

    change WebMvcConfig to implement not deprecated interface

--- src/main/java/com/epam/marketplace/config/WebMvcConfig.java
+++ src/main/java/com/epam/marketplace/config/WebMvcConfig.java
@@ -5,13 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.epam.marketplace")
-// TODO: avoid deprecated
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
