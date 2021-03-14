fcd83d2b71056725de635866242b1b18c78a1448
Bury_Bones <derkach.a.d@gmail.com>
2021-03-14 23:10:11 +0400

    add pre-destroy method for InternalProcessesCommander, to fix after-shutdown queries to the DB

--- src/main/java/com/epam/marketplace/internal/InternalProcessesCommander.java
+++ src/main/java/com/epam/marketplace/internal/InternalProcessesCommander.java
@@ -1,6 +1,7 @@
 package com.epam.marketplace.internal;
 
 import java.util.Timer;
+import javax.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -10,16 +11,21 @@ import org.springframework.stereotype.Component;
 public class InternalProcessesCommander implements ApplicationListener<ContextRefreshedEvent> {
 
   private final DealCloserTask dealCloserTask;
+  private final Timer dealCloserTimer = new Timer(true);
 
   @Autowired
   public InternalProcessesCommander(DealCloserTask dealCloserTask) {
     this.dealCloserTask = dealCloserTask;
   }
-  private final Timer dealCloserTimer = new Timer(true);
 
   @Override
   public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-    long minute = 1000*60;
+    long minute = 1000 * 60;
     dealCloserTimer.schedule(dealCloserTask, 0, minute);
+  }
+
+  @PreDestroy
+  public void shutDown() {
+    dealCloserTimer.cancel();
   }
 }
