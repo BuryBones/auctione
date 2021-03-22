7ab064e7a592802a5c386977e21e34e30bd5f5fe
Bury_Bones <derkach.a.d@gmail.com>
2021-03-22 12:56:17 +0400

    add validation steps for Item

--- src/main/java/com/epam/marketplace/services/ItemService.java
+++ src/main/java/com/epam/marketplace/services/ItemService.java
@@ -2,10 +2,15 @@ package com.epam.marketplace.services;
 
 import com.epam.marketplace.OperationResult;
 import com.epam.marketplace.dao.ItemDao;
+import com.epam.marketplace.dto.DealDto;
 import com.epam.marketplace.dto.mappers.CommonMapper;
 import com.epam.marketplace.entities.Deal;
 import com.epam.marketplace.entities.Item;
 import com.epam.marketplace.dto.ItemDto;
+import com.epam.marketplace.validation.ConstraintsValidator;
+import com.epam.marketplace.validation.logic.AbstractLogicValidator;
+import com.epam.marketplace.validation.logic.DealLogicValidator;
+import com.epam.marketplace.validation.logic.ItemLogicValidator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -18,11 +23,17 @@ public class ItemService {
   private final Logger logger = Logger.getLogger("application");
   private final ItemDao itemDao;
   private final CommonMapper mapper;
+  private final ConstraintsValidator constraintsValidator;
+  private final ItemLogicValidator itemLogicValidator;
 
   @Autowired
-  public ItemService(ItemDao itemDao, CommonMapper mapper) {
+  public ItemService(ItemDao itemDao, CommonMapper mapper,
+      ConstraintsValidator constraintsValidator,
+      ItemLogicValidator itemLogicValidator) {
     this.itemDao = itemDao;
     this.mapper = mapper;
+    this.constraintsValidator = constraintsValidator;
+    this.itemLogicValidator = itemLogicValidator;
   }
 
   public List<ItemDto> getItemsByUserId(int userId) {
@@ -44,7 +55,21 @@ public class ItemService {
   }
 
   public OperationResult createItem(ItemDto newBorn) {
-    Item newItem = mapper.getEntityFromDto(newBorn);
-    return itemDao.save(newItem);
+    OperationResult validationResult = validate(newBorn);
+    if (validationResult.getResult()) {
+      Item newItem = mapper.getEntityFromDto(newBorn);
+      return itemDao.save(newItem);
+    } else {
+      return validationResult;
+    }
+  }
+
+  public OperationResult validate(ItemDto itemDto) {
+    OperationResult constraintsValidationResult = constraintsValidator.validate(itemDto);
+    if (constraintsValidationResult.getResult()) {
+      return itemLogicValidator.validate(itemDto);
+    } else {
+      return constraintsValidationResult;
+    }
   }
 }
